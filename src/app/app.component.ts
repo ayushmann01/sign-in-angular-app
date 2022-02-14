d9c403e0b7f94af1472d9e8187a34318cfc88035
Ayushman <ayushmanraghav.ra@gmail.com>
2022-02-14 23:33:03 +0530

    initial sign-in app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'sing-in';
+
+  onEyeCLick(eye: HTMLElement,field: HTMLInputElement) {
+    eye.classList.toggle("bi-eye");
+    eye.classList.toggle("bi-eye-slash");
+
+    if(field.type === "password") field.type = "text";
+    else field.type = "password";
+  }
+
+  onTxtClick(email: HTMLInputElement, pass: HTMLInputElement, btn_submit: HTMLButtonElement) {
+      if(email.value) {
+        btn_submit.disabled = false;
+      }
+      else btn_submit.disabled = true;
+  }
 }
+
+
